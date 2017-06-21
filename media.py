aba849ee16cdfcc9451ec7a9953f0266cef1bee7
Jingyi Wang <jingyiwang@Jingyis-MacBook-Pro.local>
2017-06-20 21:39:41 -0700

    Upload the files

---
+++ media.py
@@ -0,0 +1,8 @@
+class Movie():
+    def __init__(self, movie_title, movie_time, movie_actors, movie_storyline, movie_poster, movie_trailer):
+        self.title = movie_title
+        self.mtime = movie_time
+        self.actors = movie_actors
+        self.storyline = movie_storyline
+        self.poster_image_url = movie_poster
+        self.trailer_youtube_url = movie_trailer
